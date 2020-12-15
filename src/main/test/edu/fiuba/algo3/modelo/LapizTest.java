e4d633ff82e516829a6204801dc8e29e8832a5a2
Pandamos <abrildiazmiguez99@gmail.com>
2020-12-15 14:35:26 -0300

    feat: Lapiz tiene más pruebas y pasa todas.

--- src/main/test/edu/fiuba/algo3/modelo/LapizTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/LapizTest.java
@@ -4,6 +4,8 @@ import edu.fiuba.algo3.modelo.Lapiz;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class LapizTest {
 
@@ -28,6 +30,39 @@ public class LapizTest {
         unLapiz.subirLapiz();
 
         assertTrue(unLapiz.estaLevantado());
+    }
+
+    @Test
+    public void elLapizApoyadoPinta() {
+        Posicion posicionMock = mock(Posicion.class);
+
+        //necesito que empiece en false
+        when(posicionMock.estaPintada()).thenReturn(true);
+
+        Lapiz unLapiz = new Lapiz();
+        unLapiz.bajarLapiz();
+
+        //necesito que cuando llame a esta cosa, el false cambie a true
+        unLapiz.dibujar(posicionMock);
+
+        //necesito verificar que devolvio true
+        assertTrue(posicionMock.estaPintada());
+    }
+
+    @Test
+    public void elLapizLevantadoNoPinta() {
+        Posicion posicionMock = mock(Posicion.class);
+
+        //necesito que empiece en false
+        when(posicionMock.estaPintada()).thenReturn(false);
+
+        Lapiz unLapiz = new Lapiz();
+
+        //necesito que cuando llame a esta cosa, el false cambie a true
+        unLapiz.dibujar(posicionMock);
+
+        //necesito verificar que devolvio true
+        assertFalse(posicionMock.estaPintada());
     }
 
 }
