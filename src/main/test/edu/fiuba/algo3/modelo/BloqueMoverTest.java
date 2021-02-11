4f3cd55d7b965091e7323d6f35a6e969285501dd
mateocapon <mcapon@fi.uba.ar>
2021-02-10 23:47:22 -0300

    feat: agregamos tests para bloqueMover
    Co-autored-by: gsabatino9 <gsabatino@fi.uba.ar>
    Co-autored-by: Pandamos <abrildiazmiguez99@gmail.com>

--- src/main/test/edu/fiuba/algo3/modelo/BloqueMoverTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/BloqueMoverTest.java
@@ -4,15 +4,53 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BloqueMoverTest {
-    /*
+
     @Test
     public void seCreaUnBloqueMoverConDireccionADerechaYElPersonajeDebeMoverse(){
         Posicion posicionActual = new Posicion(0,0);
-        Personaje personaje = new Personaje(posicionActual);
+        Lapiz lapiz = new Lapiz(new Dibujo());
+        Personaje personaje = new Personaje(posicionActual, lapiz);
         BloqueMover bloque = new BloqueMover(Direccion.obtenerDerecha());
 
         bloque.ejecutar(personaje);
-        assertTrue(personaje.devolverPosicion().estaOcupada());
+        Posicion derecha = new Posicion(1,0);
+        assertTrue(personaje.devolverPosicion().equals(derecha));
     }
-     */
+
+    @Test
+    public void seCreaUnBloqueMoverConDireccionAIzquierdaYElPersonajeDebeMoverse(){
+        Posicion posicionActual = new Posicion(0,0);
+        Lapiz lapiz = new Lapiz(new Dibujo());
+        Personaje personaje = new Personaje(posicionActual, lapiz);
+        BloqueMover bloque = new BloqueMover(Direccion.obtenerIzquierda());
+
+        bloque.ejecutar(personaje);
+        Posicion izquierda = new Posicion(-1,0);
+        assertTrue(personaje.devolverPosicion().equals(izquierda));
+    }
+
+    @Test
+    public void seCreaUnBloqueMoverConDireccionArribaYElPersonajeDebeMoverse(){
+        Posicion posicionActual = new Posicion(0,0);
+        Lapiz lapiz = new Lapiz(new Dibujo());
+        Personaje personaje = new Personaje(posicionActual, lapiz);
+        BloqueMover bloque = new BloqueMover(Direccion.obtenerArriba());
+
+        bloque.ejecutar(personaje);
+        Posicion arriba = new Posicion(0,1);
+        assertTrue(personaje.devolverPosicion().equals(arriba));
+    }
+
+    @Test
+    public void seCreaUnBloqueMoverConDireccionAbajoYElPersonajeDebeMoverse(){
+        Posicion posicionActual = new Posicion(0,0);
+        Lapiz lapiz = new Lapiz(new Dibujo());
+        Personaje personaje = new Personaje(posicionActual, lapiz);
+        BloqueMover bloque = new BloqueMover(Direccion.obtenerAbajo());
+
+        bloque.ejecutar(personaje);
+        Posicion abajo = new Posicion(0,-1);
+        assertTrue(personaje.devolverPosicion().equals(abajo));
+    }
+
 }
