9d31f469d0b13e03a70528c265533d58eb58ed39
Pandamos <abrildiazmiguez99@gmail.com>
2021-02-17 17:46:30 -0300

    chore: iniciando la interfaz gráfica.

--- src/main/java/edu/fiuba/algo3/App.java
+++ src/main/java/edu/fiuba/algo3/App.java
@@ -1,10 +1,19 @@
 package edu.fiuba.algo3;
 
 import javafx.application.Application;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * JavaFX App
@@ -13,13 +22,38 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
+        SplitPane splitPane = new SplitPane();
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        Button ejecutar = new Button("Ejecutar");
+        Button guardar = new Button("Guardar");
+        HBox bottomControl = new HBox(ejecutar, guardar);
+
+        Button bloqueDerecha = new Button("Mover Derecha");
+        Button bloqueIzqueirda = new Button("Mover Izquierda");
+        Button bloqueArriba = new Button("Mover Arriba");
+        Button bloqueAbajo = new Button("Mover Abajo");
+        Button bloqueLevantar = new Button("Levantar Lapiz");
+        Button bloqueApoyar = new Button("Apoyar Lapiz");
+        HBox bottomControl2 = new HBox(bloqueDerecha, bloqueIzqueirda, bloqueArriba, bloqueAbajo, bloqueLevantar, bloqueApoyar);
+
+        Button bloqueInvertir = new Button("Bloque Invertir");
+        Button bloqueRepetir = new Button("Bloque Repetir");
+        HBox bottomControl3 = new HBox(bloqueInvertir, bloqueRepetir);
+
+        VBox leftControl  = new VBox(new Label("Secuencia de Bloques"), bottomControl, new Label("Bloques disponibles"), new Label("Bloques simples"), bottomControl2, new Label("Bloques compuestos"), bottomControl3);
+        VBox rightControl = new VBox(new Label("Donde se mueve el personaje"));
+
+//      leftControl.setSpacing(300);
+
+        splitPane.getItems().addAll(leftControl, rightControl);
+
+        Scene scene = new Scene(splitPane, 800, 700);
+
         stage.setScene(scene);
+        stage.setTitle("JavaFX App");
+
         stage.show();
+
     }
 
     public static void main(String[] args) {
