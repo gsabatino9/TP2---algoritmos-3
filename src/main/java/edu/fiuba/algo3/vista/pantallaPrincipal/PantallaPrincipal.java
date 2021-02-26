e2f6a33b4d1c6f492a827a710769ea532bd6e520
Pandamos <abrildiazmiguez99@gmail.com>
2021-02-26 20:59:38 -0300

    chore: agrego pruebas a personaje

--- src/main/java/edu/fiuba/algo3/vista/pantallaPrincipal/PantallaPrincipal.java
+++ src/main/java/edu/fiuba/algo3/vista/pantallaPrincipal/PantallaPrincipal.java
@@ -3,26 +3,22 @@ package edu.fiuba.algo3.vista.pantallaPrincipal;
 import edu.fiuba.algo3.vista.evento.BloqueEventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class PantallaPrincipal extends BorderPane {
 
     private VBox botoneras;
+    private GridPane dibujo;
     private VBox secuencia;
     private ScrollPane secuenciaBloques;
     private VBox contenedor;
     private BarraMenu barraMenu;
     private Scene escenaJuego;
-    private StackPane tablero;
 
 
 
@@ -58,25 +54,9 @@ public class PantallaPrincipal extends BorderPane {
         Image img = new Image("Ash.jpeg");
         view.setImage(img);
 
-        Image img2 = new Image("Nnnn.png");
-        ImageView view4 = new ImageView();
-        view4.setFitWidth(10);
-        view4.setFitHeight(10);
-        view4.setImage(img2);
-
-        ImageView view5 = new ImageView();
-        view5.setFitWidth(10);
-        view5.setFitHeight(10);
-        view5.setImage(img2);
-
-        ImageView view6 = new ImageView();
-        view6.setFitWidth(10);
-        view6.setFitHeight(10);
-        view6.setImage(img2);
-
         ImageView view2 = new ImageView();
 
-        GridPane dibujo = new GridPane();
+        this.dibujo = new GridPane();
         dibujo.setMinWidth(800);
         dibujo.setMinHeight(600);
         dibujo.setHgap(50);
@@ -86,36 +66,11 @@ public class PantallaPrincipal extends BorderPane {
         dibujo.setGridLinesVisible(true);
         dibujo.setAlignment(Pos.CENTER);
 
+
         Image imagen = new Image("Fond.jpg");
         BackgroundImage imagenDeFondo = new BackgroundImage(imagen, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(1000, 800, false, false, false, false));
-
-        ImageView view3 = new ImageView();
-
-        GridPane dibujo2 = new GridPane();
-        dibujo2.setMinWidth(771);
-        dibujo2.setMinHeight(549);
-        dibujo2.setHgap(1);
-        dibujo2.setVgap(1);
-        dibujo2.add(view3, 771, 549);
-        dibujo2.setGridLinesVisible(false);
-
-        for(int i = 0; i < 4; i++){
-            ImageView viewAux = new ImageView();
-            viewAux.setFitWidth(5);
-            viewAux.setFitHeight(5);
-            viewAux.setImage(img2);
-
-            dibujo2.add(viewAux, 175 + i, 175);
-        }
-
-
-        dibujo2.setAlignment(Pos.CENTER);
-
-
-        this.tablero = new StackPane();
-        tablero.getChildren().addAll(dibujo2, dibujo);
-        tablero.setBackground(new Background(imagenDeFondo));
-        this.setCenter(tablero);
+        dibujo.setBackground(new Background(imagenDeFondo));
+        this.setCenter(dibujo);
     }
 
     public void setContenedor(){
@@ -133,7 +88,6 @@ public class PantallaPrincipal extends BorderPane {
 
         this.secuencia = new VBox();
         this.secuenciaBloques = new ScrollPane(this.secuencia);
-        this.secuenciaBloques.setMinSize(170, 25);
         contenedor.getChildren().add(this.secuenciaBloques);
     }
 
--- src/main/test/edu/fiuba/algo3/modelo/PersonajeTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/PersonajeTest.java
@@ -2,21 +2,143 @@ package edu.fiuba.algo3.modelo;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Objects;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class PersonajeTest {
 
-    /*
-    @Test
-    public void PersonajeSeCreaPorDefectoConElLapizLevantadoAlMoverDerechaNoSePinta() {
+    @Test //REVISAR COMO COMPARAR CLASES
+    public void PersonajeSeCreaPorDefectoConElLapizLevantado() {
         Posicion actual = new Posicion(0,0);
-        Personaje personaje = new Personaje(actual);
-        Direccion derecha = Direccion.obtenerDerecha();
-        personaje.mover(derecha);
-        assertFalse(actual.estaPintada());
+        Dibujo dibujo = new Dibujo();
+        Lapiz lapiz = new Lapiz(dibujo);
+        Personaje personaje = new Personaje(actual, lapiz);
+
+        assertSame(personaje.obtenerEstado().getClass(), lapiz.obtenerEstado().getClass());
     }
 
+    @Test //REVISAR COMO COMPARAR CLASES
+    public void PersonajePuedeCambiarSuTipoDeLapiz() {
+        Posicion actual = new Posicion(0,0);
+        Dibujo dibujo = new Dibujo();
+        Lapiz lapiz = new Lapiz(dibujo);
+        Personaje personaje = new Personaje(actual, lapiz);
+        Lapiz lapizNuevo = new Lapiz(dibujo);
+
+        personaje.cambiarEstadoDelLapiz(new LapizApoyado());
+        assertNotSame(personaje.obtenerEstado().getClass(), lapizNuevo.obtenerEstado().getClass());
+    }
+
+
+    @Test
+    public void PersonajePuedeMoverseADerecha() {
+        Posicion actual = new Posicion(0,0);
+        Posicion posicionDerecha = new Posicion(1, 0);
+        Dibujo dibujo = new Dibujo();
+        Lapiz lapiz = new Lapiz(dibujo);
+        Personaje personaje = new Personaje(actual, lapiz);
+
+        Direccion derecha = Direccion.obtenerDerecha();
+        personaje.mover(derecha);
+
+        assertTrue(posicionDerecha.equals(personaje.devolverPosicion()));
+    }
+
+
+    @Test
+    public void PersonajePuedeAgregarBloquePersonalizado() throws BloquePersonalizadoYaExisteException {
+        Posicion actual = new Posicion(0,0);
+        Posicion posicionDerecha = new Posicion(1, 0);
+        Dibujo dibujo = new Dibujo();
+        Lapiz lapiz = new Lapiz(dibujo);
+        Personaje personaje = new Personaje(actual, lapiz);
+
+
+        BloqueMover bloqueArriba = new BloqueMover(Direccion.obtenerArriba());
+        BloqueMover bloqueDerecha = new BloqueMover(Direccion.obtenerDerecha());
+
+        Algoritmo algoritmoPersonalizado = new Algoritmo();
+        algoritmoPersonalizado.agregarBloque(bloqueArriba);
+        algoritmoPersonalizado.agregarBloque(bloqueDerecha);
+
+        personaje.agregarBloque(algoritmoPersonalizado, "Arriba y a derecha");
+        assertNotNull(personaje.obtenerAlgoritmo("Arriba y a derecha"));
+    }
+
+    @Test
+    public void PersonajeNoPuedeAgregarUnBloqueQueYaEstaGuardado() throws BloquePersonalizadoYaExisteException {
+        Posicion actual = new Posicion(0,0);
+        Dibujo dibujo = new Dibujo();
+        Lapiz lapiz = new Lapiz(dibujo);
+        Personaje personaje = new Personaje(actual, lapiz);
+
+
+        BloqueMover bloqueArriba = new BloqueMover(Direccion.obtenerArriba());
+        BloqueMover bloqueDerecha = new BloqueMover(Direccion.obtenerDerecha());
+
+        Algoritmo algoritmoPersonalizado = new Algoritmo();
+        algoritmoPersonalizado.agregarBloque(bloqueArriba);
+        algoritmoPersonalizado.agregarBloque(bloqueDerecha);
+
+        personaje.agregarBloque(algoritmoPersonalizado, "Arriba y a derecha");
+        assertThrows(BloquePersonalizadoYaExisteException.class, () ->  personaje.agregarBloque(algoritmoPersonalizado,"Arriba y a derecha"));
+
+    }
+
+    @Test
+    public void PersonajeNoPuedeAgregarUnBloqueQueTieneNombreNoDisponible() throws BloquePersonalizadoYaExisteException {
+        Posicion actual = new Posicion(0,0);
+        Dibujo dibujo = new Dibujo();
+        Lapiz lapiz = new Lapiz(dibujo);
+        Personaje personaje = new Personaje(actual, lapiz);
+
+
+        BloqueMover bloqueArriba = new BloqueMover(Direccion.obtenerArriba());
+        BloqueMover bloqueDerecha = new BloqueMover(Direccion.obtenerDerecha());
+
+        Algoritmo algoritmoPersonalizado = new Algoritmo();
+        algoritmoPersonalizado.agregarBloque(bloqueArriba);
+        algoritmoPersonalizado.agregarBloque(bloqueDerecha);
+
+        Algoritmo algoritmoPersonalizado2 = new Algoritmo();
+        algoritmoPersonalizado2.agregarBloque(bloqueArriba);
+        algoritmoPersonalizado2.agregarBloque(bloqueDerecha);
+
+        personaje.agregarBloque(algoritmoPersonalizado, "Arriba y a derecha");
+        assertThrows(BloquePersonalizadoYaExisteException.class, () ->  personaje.agregarBloque(algoritmoPersonalizado2,"Arriba y a derecha"));
+    }
+
+    @Test
+    public void PersonajeNoPuedeAgregarUnBloqueQueTieneNombreNoDisponibleAunqueTengaOtrosBloques() throws BloquePersonalizadoYaExisteException {
+        Posicion actual = new Posicion(0,0);
+        Dibujo dibujo = new Dibujo();
+        Lapiz lapiz = new Lapiz(dibujo);
+        Personaje personaje = new Personaje(actual, lapiz);
+
+
+        BloqueMover bloqueArriba = new BloqueMover(Direccion.obtenerArriba());
+        BloqueMover bloqueDerecha = new BloqueMover(Direccion.obtenerDerecha());
+
+        Algoritmo algoritmoPersonalizado = new Algoritmo();
+        algoritmoPersonalizado.agregarBloque(bloqueArriba);
+        algoritmoPersonalizado.agregarBloque(bloqueDerecha);
+
+        BloqueMover bloqueArriba2 = new BloqueMover(Direccion.obtenerArriba());
+        BloqueMover bloqueDerecha2 = new BloqueMover(Direccion.obtenerDerecha());
+
+        Algoritmo algoritmoPersonalizado2 = new Algoritmo();
+        algoritmoPersonalizado2.agregarBloque(bloqueArriba2);
+        algoritmoPersonalizado2.agregarBloque(bloqueDerecha2);
+
+        personaje.agregarBloque(algoritmoPersonalizado, "Arriba y a derecha");
+        assertThrows(BloquePersonalizadoYaExisteException.class, () ->  personaje.agregarBloque(algoritmoPersonalizado2,"Arriba y a derecha"));
+    }
+
+
+
+    /*
     @Test
     public void PersonajeSeCreaPorDefectoConElLapizLevantadoAlMoverDerechaPosicionSeEncuentraDesocupada() {
 
